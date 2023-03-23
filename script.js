281f22fd10de23ad5b02eca95dc61dbba52d1ee2
Fizzah Sikander <123269709+FizzahSikander@users.noreply.github.com>
2023-03-23 20:28:20 +0100

    summary added

--- script.js
+++ script.js
@@ -17,20 +17,28 @@ const account = {
   },
   getList: function () {
     console.log(this);
-    alert(`Income: ${this.income},Expense:${this.expenses}`);
+    let incomes = "";
+    this.income.forEach(function (income, index) {
+      incomes += "Income nr " + (index + 1) + ": " + income.addedIncome + "\n";
+    });
+    let expense = "";
+    this.expenses.forEach(function (Expenses, index) {
+      expense += "Expense nr " + (index + 1) + ": " + Expenses.ammount + "\n";
+    });
+    alert(`Income:${incomes},Expense:${expense}`);
   },
-  //getSummary: function () {
-  //console.log(this);
-  //let totalIncome = 0;
-  //let totalExpenses = 0;
-  //this.income.foreach(function (income) {
-  //totalIncome = totalIncome + income.addedIncome;
-  //});
-  //this.expenses.foreach(function (expense) {
-  //totalExpenses = totalExpenses + expense.ammount;
-  //});
-  //alert(`Fizzah ,your total income is  ${totalIncome},total expense is  ${totalExpenses} ,balances are: ${totalIncome - totalExpenses}`);
-  //},
+  getSummary: function () {
+    console.log(this);
+    let totalIncome = 0;
+    let totalExpenses = 0;
+    this.income.forEach(function (income) {
+      totalIncome = totalIncome + income.addedIncome;
+    });
+    this.expenses.forEach(function (expense) {
+      totalExpenses = totalExpenses + expense.ammount;
+    });
+    alert(`Fizzah ,your total income is  ${totalIncome},total expense is  ${totalExpenses} ,balances are: ${totalIncome - totalExpenses}`);
+  },
 };
 
 function menu() {
@@ -59,114 +67,3 @@ function menu() {
   }
 }
 menu();
-
-
-
-
-// create an account object
-
-// the object should hold all of our logic
-// THE ACCOUNT OBJECT SHOULD HAVE THE FOLLWOING PROPERTIES:
-// name: the account holders name, should be a string
-// expenses: an array that should hold all of your expenses, can be empty from start
-// income: an array that should hold all of your income, can be empty from start
-//addExpenses: a function that vill add an expense to the expenses array
-// function calcExpenses(){ const addExpenses= newExpense+expenses;return addExpenses;}
-//calcExpenses();
-// addIncome: a function that vill add an income to the income array
-//function calcIncome(){
-//const addIncome= newIncome+income;
-//return addIncome;}
-//calcIncome();
-// listAllExpenses: a function that should list all the expenses in the expenses array
-//function calcnewExpenses(){
-//const listAllExpenses= newExpense+ addExpenses;
-//return listAllExpenses;
-//}
-//newExpense();
-// getSummary: a function that should summarize your total balances. It should show your total
-// income, your total expenses and also calculate your current balances (income - expenses).
-
-//const account = {
-//name: "Fizzah",
-//getName: function () {
-//alert(`Account name: ${this.name}`);
-//},
-//expenses:"",
-//income:,
-//addExpenses:,
-//addIncome:,
-// listAllExpenses:,
-//  getSummary:,
-//};
-
-// create a function called menu()
-
-// the function menu() should only be responsible to show the different choices
-// for the user.
-
-// The menu() function should display the menu with different choices for the user
-// depending on what the user selects from the menu you should call on the correct
-// method or poperty from the account object. You can use a switch or if/else statement for your
-// different menu choices. Please motivate you choice in a comment.
-
-
-
-
-
-//const expenses = [];
-//const newExpense = parseFloat(prompt("how much your expense?"));
-//const addExpenses = expenses.concat(newExpense);
-
-//const income = [];
-//const newIncome = parseFloat(prompt("how much your income?"));
-//const addIncome = income.concat(newIncome);
-//
-
-//const account = {
-  //name: "Fizzah",
-  //getName: function () {
-  //alert(`Account name: ${this.name}`);
-  //},
-  //addExpenses:,
-  //addIncome:,
-  //listAllExpenses:,
-  //GetSummary:,
-  //};
-  //console.log(account);
-
-
-// CODE TO HELP YOU GET STARTED
-
-// Here are some code snippets to help you get started. You need to place them at the correct
-// place in your code but this is the logic for some parts of your assignment.
-
-// if we want to take an input from the user in a quite nice way we can use the propmt() function
-// the prompt will show a window to the user where we can add a message and it will also display
-// an input field where the user can type something. In order to make this work a bit easier
-// we need to wrap the propmt() in a function called parseFloat(). Why is that? You need to do some
-// googling anf the put your answer in a comment in your code.
-// the syntax you can use looks like this:
-//const amount = parseFloat(prompt("How much was your expense?"));
-// the answer that the user will put in the input box will get stored in the variable amount.
-
-// HINT: you will need to use array methods in this assignment and there is one very basic one
-// that is important. It is the method for adding something to an existing array, which one is that?
-//IS it push method?or concat method?
-// In order to show all elements in an array we need to loop through it. In this assignment a
-// forEach loop would be good to use. If you want to display the outcome of each element in the
-// array on separate lines you can add:
-//("\n");
-// for example:
-//alert("This is a message " + "\n" + "with a new line");
-// to your ouput. This will make the nes output on a new line.
-
-// to make the menu look nice we should not console.log any output. Instead we shoul use either alert()
-// or propmt()
-// we use alert() when we just want to display something to the user, for example a message:
-//alert("This is a message in an alert box!");
-// we use the prompt() when we want to ask the user to do something cause the prompt()
-// will display any message that we want and also display an input field where the user
-// can type an input. Like this:
-//const age = parseFloat(propmpt("How old are you?"));
-// whatever the user types in the input field will be stored in the variable age.
